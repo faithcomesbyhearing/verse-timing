e6da25a4037de4bd80a4e3aab3355f146706a2b8
spanta28 <spanta@mrn.org>
2019-11-10 23:26:19 -0700

    Add files via upload

--- create_cue_file.py
+++ create_cue_file.py
@@ -9,25 +9,33 @@ required_args.add_argument(
     '-o', required=True, nargs=1, type=str, help='Output file name')
 
 optional_args=parser.add_argument_group('optional arguments')
-optional_args.add_argument('-add_info', action='store_true', help='Add additional info: filesetid,bookname,chapter number')
+optional_args.add_argument('-fileset', nargs=1, type=str,help='custom fileset id')
+optional_args.add_argument('-book', nargs=1, type=str, help='custom book id')
+optional_args.add_argument('-chapter', nargs=1, type=str,help='custom chapter id')
+optional_args.add_argument('-noheader', action='store_true', help='Remove header: fileset,book,chapter,db,sId,sBegin,sEnd')
 
 args = parser.parse_args()
-input_cue_file=args.i[0]
-output_cue_file=args.o[0]
-add_info=args.add_info
+input_file=args.i[0]
+output_file=args.o[0]
 
 
-read_file=open(input_cue_file,'r')
-write_file=open(output_cue_file,'w')
+# Open read and write files
+read_file=open(input_file,'r')
+write_file=open(output_file,'w')
 
-#Add 4 lines if add_info is True
-if add_info:
-    filename = input_cue_file.split('/')[-1]
-    input = re.compile('\_+').split(filename.split('.')[0])
-    file_setid = input[1] + input[0]
-    book_name = input[-3]
-    chapter_num = input[-2]
-    write_file.write(file_setid + '\n' + book_name + '\n' + chapter_num + '\n')
+
+# Write header
+if not(args.noheader):write_file.write('fileset,book,chapter,line,qtime\n')
+
+#Fill the header values and overwrite custom option
+filename=input_file.split('/')[-1]
+input=re.compile('\_+').split(filename.split('.')[0])
+if args.fileset is not None:file_setid=args.fileset[0]
+else:file_setid=input[1]+input[0]
+if args.book is not None: book_name=args.book[0]
+else:book_name=input[-2]
+if args.chapter is not None:chapter_num=args.chapter[0]
+else:chapter_num=input[-3]
 
 # Adjust decoding patterm in .clt
 lines=read_file.readlines()
@@ -44,8 +52,8 @@ while line_number_id<=len(lines):
     start_time=round(float(lines[start_time_id].strip())/bits_per_second,3)
     line_audio_file = (lines[line_number_id]).strip()
     line_number=(re.compile('\_+').split(line_audio_file.split('.')[0]))[-1]
-    write_file.write(str(start_time)+','+str(line_number)+'\n')
+    write_file.write(file_setid + ',' + book_name + ',' + chapter_num +',')
+    write_file.write(str(line_number)+','+str(start_time)+'\n')
     start_time_id+=increment
     line_number_id+=increment
-
 
--- create_silence_file.py
+++ create_silence_file.py
@@ -1,7 +1,8 @@
-import argparse
+import argparse,pydub.silence as sil,os,re
+from pydub import AudioSegment
 
 parser = argparse.ArgumentParser(
-        description='This function gets the silence boundaries given an input audio file')
+        description='This function gets the silence boundaries given an input audio file in the foll. format fileset,book,chapter,db,sId,sBegin,sEnd')
 required_args = parser.add_argument_group('required arguments')
 required_args.add_argument(
     '-i', required=True, nargs=1, type=str, help='Input audio file')
@@ -11,42 +12,46 @@ required_args.add_argument(
 optional_args=parser.add_argument_group('optional arguments')
 optional_args.add_argument('-db', default=[5], nargs=1, type=int, help='Threshold decibel level')
 optional_args.add_argument('-sl', default=[300], nargs=1, type=int, help='Min. silence length(secs)')
-optional_args.add_argument('-add_info', action='store_true', help='Add additional info: filesetid,bookname,chapter number')
+optional_args.add_argument('-fileset', nargs=1, type=str,help='custom fileset id')
+optional_args.add_argument('-book', nargs=1, type=str, help='custom book id')
+optional_args.add_argument('-chapter', nargs=1, type=str,help='custom chapter id')
+optional_args.add_argument('-noheader', action='store_true', help='Remove header: fileset,book,chapter,db,sId,sBegin,sEnd')
+
 
 args = parser.parse_args()
-input_audio_file=args.i[0]
-output_silence_file=args.o[0]
+input_file=args.i[0]
+output_file=args.o[0]
 decibels=args.db[0]
 min_sil_len=args.sl[0]
-add_info=args.add_info
+
+#Open the write file
+if os.path.exists(output_file): os.remove(output_file)
+write_file=open(output_file,'w')
+
+# Write header
+if not(args.noheader):write_file.write('fileset,book,chapter,db,sId,sBegin,sEnd\n')
+
+#Fill the header values and overwrite custom option
+filename=input_file.split('/')[-1]
+input=re.compile('\_+').split(filename.split('.')[0])
+if args.fileset is not None:file_setid=args.fileset[0]
+else:file_setid=input[-1]
+if args.book is not None: book_name=args.book[0]
+else:book_name=input[-2]
+if args.chapter is not None:chapter_num=args.chapter[0]
+else:chapter_num=input[-3]
 
 
-
-import pydub.silence as sil,os,re
-from pydub import AudioSegment
-
-
-
-sound = AudioSegment.from_mp3(input_audio_file)
+sound = AudioSegment.from_mp3(input_file)
 dBFS = sound.dBFS
 silence_boundaries = sil.detect_silence(sound, min_silence_len=min_sil_len, silence_thresh=dBFS - decibels)
-if os.path.exists(output_silence_file): os.remove(output_silence_file)
-silence_file=open(output_silence_file,'w')
 
-#Add 4 lines if add_info is True
-if add_info:
-    filename=input_audio_file.split('/')[-1]
-    input=re.compile('\_+').split(filename.split('.')[0])
-    file_setid=input[-1]
-    book_name=input[-2]
-    chapter_num=input[-3]
-    silence_file.write('# -'+str(decibels)+'\n')
-    silence_file.write(file_setid + '\n'+book_name + '\n'+chapter_num + '\n')
 
 line_inc=0
 for boundaries in silence_boundaries:
     line_inc+=1
     boundaries=[x/1000 for x in boundaries]
-    silence_file.write("{0},{1},{2}\n".format(line_inc,boundaries[0],boundaries[1]))
+    write_file.write(file_setid + ',' + book_name + ',' + chapter_num + ',' + '-' + str(decibels)+',')
+    write_file.write("{0},{1},{2}\n".format(line_inc,boundaries[0],boundaries[1]))
     # print("{0} {1} silence".format(boundaries[0],boundaries[1]))
-silence_file.close()
+write_file.close()
